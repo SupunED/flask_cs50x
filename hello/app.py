a1421d2320adb046caef7ccf0212537bd2ac12a5
supun <supunpramudithajayawardhana@gmail.com>
2025-07-21 23:53:34 +0530

    Using placeholders

--- hello/app.py
+++ hello/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    html = render_template("index.html")
-    return html
+    name = request.args["name"]
+    return render_template("index.html", placeholder=name)
