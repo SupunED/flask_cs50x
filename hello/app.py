d39e660759ab249ed7e5f8b596544a4e24c2a242
supun <supunpramudithajayawardhana@gmail.com>
2025-07-22 00:00:22 +0530

    simplify error handling using request.args.get method

--- hello/app.py
+++ hello/app.py
@@ -4,9 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    if "name" in request.args:
-        name = request.args["name"]
-    else:
-        name = "world"
+    name = request.args.get("name", "world")
         
     return render_template("index.html", placeholder=name)
