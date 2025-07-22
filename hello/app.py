3cd06e67c47dad0458c6f63ffaa245cbb6a17a05
supun <supunpramudithajayawardhana@gmail.com>
2025-07-22 20:54:44 +0530

    using conditionals inside templates

--- hello/app.py
+++ hello/app.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        name = request.form.get("name", "world")
-        return render_template("greet.html", name=name)
+        return render_template("greet.html", name=request.form.get("name", "world"))
     else:
         return render_template("index.html")
