a66688c2125e027bf3e0a2aa6c4964748fd9ba65
supun <supunpramudithajayawardhana@gmail.com>
2025-07-23 10:19:07 +0530

    Server side data validation and handling

--- froshims/app.py
+++ froshims/app.py
@@ -2,13 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+SPORTS = [
+    "Basketball",
+    "Soccer",
+    "Ultimate Frisbee",
+]
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sports=SPORTS)
 
 @app.route("/register", methods=["POST"])
 def register():
-    if not request.form.get("name") or not request.form.get("sport"):
+    if not request.form.get("name") or request.form.get("sport"):
         return render_template("failure.html")
-    
     return render_template("success.html")
